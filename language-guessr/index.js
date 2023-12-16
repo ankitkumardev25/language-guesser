4fb3f333020c84443d3c67f2998d1078b90ddb4d
ankitkumardev25 <ankitkumar1999825@gmail.com>
2023-12-16 23:57:39 +0530

    commit 3

--- language-guessr/index.js
+++ language-guessr/index.js
@@ -7,13 +7,15 @@ let input = process.argv[2];
 const langCode = franc(input);
 if (langCode === 'und') {
   console.log(
-    'could not understand what language that is.. please try again..!!'.red
+    'Could not understand what language that is.. please try again..!!'.red
   );
 } else {
   const lang = langs.where('3', langCode);
   if (lang) {
     console.log(lang.name);
   } else {
-    console.log('error while finding out'.red);
+    console.log(
+      `error while finding out the language for this code ${langCode}`.red
+    );
   }
 }
